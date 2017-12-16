636f1229e44ca9e341ff8a63a720f30df786c4b3
nine <ninebiscuit@outlook.com>
2017-12-16 16:44:43 +0800

    composer update

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -1,7 +1,13 @@
 <?php
 namespace App\Models;
-
 use Illuminate\Datebase\Eloquent\Model;
+
+
+if(class_exists('Illuminate\Datebase\Eloquent\Model')){
+    echo 'exists';
+}else{
+    echo 'no';
+}
 
 class Student extends Model {
     public $timestamps = false;
